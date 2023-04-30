612bae80f085c8d26b9e09aa54e70f802e291135
Chamika Madusanka <isurumadusanka.tmc@gmail.com>
2023-04-30 18:43:07 +0530

    creating router path between product page using url slug parameter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import './App.css';
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Link</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <h1>Product List</h1>
+        <div className="Products">
+          {data.products.map((product) => (
+            <div className="Product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="Product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
