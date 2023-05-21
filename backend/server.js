62f71dacbc4124321af16ccdda57114a4b6c5e30
Chamika Madusanka <isurumadusanka.tmc@gmail.com>
2023-05-21 17:59:55 +0530

    created monogo database on atlas server and on local computer, server.js connet to the database. install mongoose and dotenv packages,

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to DataBase');
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 const app = express();
 app.get('/api/product', (req, res) => {
